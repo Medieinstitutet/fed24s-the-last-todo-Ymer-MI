c9bcd3f7d7ee97319875e38cac62f587c2a83ce3
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2025-05-05 15:03:23 +0200

    Updated type to satisfy TS
    Changes to be committed:
    	modified:   src/helpers/taskStorage.ts

--- src/helpers/taskStorage.ts
+++ src/helpers/taskStorage.ts
@@ -2,11 +2,17 @@ import { Task } from "../models/Task";
 
 const TASK_STORAGE_KEY = 'tasks';
 
+type StoredTask = {
+    id: string
+    name: string
+    done: boolean
+}
+
 export const taskStorage = {
     save: (tasks: Task[]) => { localStorage.setItem(TASK_STORAGE_KEY, JSON.stringify(tasks)) },
     get: () => {
         const lsStr = localStorage.getItem(TASK_STORAGE_KEY)
 
-        return !lsStr ? undefined : (JSON.parse(lsStr) as Task[]).map(t => new Task(t.name, t.done).setID(t.id))
+        return !lsStr ? undefined : (JSON.parse(lsStr) as StoredTask[]).map(t => new Task(t.name, t.done).setID(t.id))
     } 
 }
